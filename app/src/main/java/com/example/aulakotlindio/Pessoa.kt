3ea8b3b621a6d5a9f6b2e44e55cce4787984b750
RFelixRocha <felixrochaeso@gmail.com>
2021-03-05 22:52:37 -0400

    Aplicado o private aos metodos set da class Pessoa

--- app/src/main/java/com/example/aulakotlindio/Pessoa.kt
+++ app/src/main/java/com/example/aulakotlindio/Pessoa.kt
@@ -3,11 +3,15 @@ package com.example.aulakotlindio
 class Pessoa {
     var nome : String = "Raimundo Felix"
     var cpf : String = "000.000.000-00"
+
+    private set
 }
 
 fun main(){
+
     var pessoa = Pessoa()
 
+    println(pessoa)
     println(pessoa.nome)
     println(pessoa.cpf)
 
