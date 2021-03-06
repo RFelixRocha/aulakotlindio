3b36a3b840f96431a4cd217ed3cd36053272111d
RFelixRocha <felixrochaeso@gmail.com>
2021-03-05 23:50:11 -0400

    Print das informações da class ClienteTipo

--- app/src/main/java/com/example/aulakotlindio/filialbanco/ClienteEnum.kt
+++ app/src/main/java/com/example/aulakotlindio/filialbanco/ClienteEnum.kt
@@ -1,2 +1,13 @@
 package com.example.aulakotlindio.filialbanco
 
+import com.example.aulakotlindio.ClienteTipo
+
+fun main(){
+    ClienteTipo.values().forEach { elemento -> println("${elemento.name} - $elemento")
+
+    }
+
+    ClienteTipo.values().forEach { println("${it.name} - $it")
+
+    }
+}
